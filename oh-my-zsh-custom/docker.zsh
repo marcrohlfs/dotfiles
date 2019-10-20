a4e7740ecb93e5ae974b0c1a04bae5c711b196ca
Marc Rohlfs <pomarize@gmail.com>
2019-10-21 00:53:40 +0200

    Move custom Docker aliases to new oh-my-zsh-custom dir
    Create `docker.zsh` in `oh-my-zsh-custom` dir and move the custom Docker
    aliases there.

---
+++ oh-my-zsh-custom/docker.zsh
@@ -0,0 +1,19 @@
+# Docker aliases
+
+alias d='docker'
+alias dim='docker image'
+alias dimp='docker image prune'
+alias dims='docker images'
+alias dkl='docker kill'
+alias dlg='docker logs'
+alias dlgf='docker logs --follow'
+alias dps='docker ps'
+alias dpsa='docker ps --all'
+alias drm='docker rm'
+alias drmi='docker rmi'
+alias drn='docker run'
+alias dsp='docker stop'
+alias dst='docker start'
+
+alias dc='docker-compose'
+alias dcdnv='docker-compose down -v'
