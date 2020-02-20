2d274f5e72bfbfc7d66accf1acaa77da6b86a9d5
Marc Rohlfs <pomarize@gmail.com>
2020-02-20 01:23:52 +0100

    Disable default Git completions
    Otherwise _Oh My Zsh_ Git completions won't work. Moving the completion
    scripts away from `/usr/local/share/zsh/site-functions` is more like a
    hack than a solution, but I didn't find another way since _Homebrew_
    removed the `--without-completions` option.

--- oh-my-zsh-custom/plugins/git/git.plugin.zsh
+++ oh-my-zsh-custom/plugins/git/git.plugin.zsh
@@ -6,3 +6,9 @@ local git_plugin_dir=${0:h}
 source ${git_plugin_dir}/git.functions.zsh
 source ${git_plugin_dir}/git.aliases.zsh
 source ${git_plugin_dir}/git.config.zsh
+
+# Disable completions from (Homebrew) Git installations
+mkdir -p /usr/local/share/zsh/site-functions/disabled
+for GIT_COMP_FILE in $( ls -1 /usr/local/share/zsh/site-functions | grep git ); do
+  mv -fv /usr/local/share/zsh/site-functions/${GIT_COMP_FILE} /usr/local/share/zsh/site-functions/disabled/${GIT_COMP_FILE}
+done
