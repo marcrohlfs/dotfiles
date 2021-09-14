8b9b3496a70be05703e972f4809cad91e813471e
Marc Rohlfs <marc.rohlfs@coremedia.com>
2021-09-14 16:30:28 +0200

    Install and link tooling.cli-scripts
    * ci-ssh
    * ci-trigger
    * pr-integrate

--- oh-my-zsh-custom/plugins/coremedia/coremedia.plugin.zsh
+++ oh-my-zsh-custom/plugins/coremedia/coremedia.plugin.zsh
@@ -8,4 +8,10 @@ plugin_dir=${0:h}
 source "${plugin_dir}/aws.zsh"
 source "${plugin_dir}/maven.zsh"
 
+# Add scripts to PATH
+ln -sfn ${plugin_dir}/checkout/ci-ssh/ci-ssh.bash ${plugin_dir}/bin/ci-ssh
+ln -sfn ${plugin_dir}/checkout/ci-trigger/ci-trigger.bash ${plugin_dir}/bin/ci-trigger
+ln -sfn ${plugin_dir}/checkout/pr-integrate/pr-integrate.bash ${plugin_dir}/bin/pr-integrate
+export PATH="${plugin_dir}/bin:${PATH}"
+
 unset plugin_dir
