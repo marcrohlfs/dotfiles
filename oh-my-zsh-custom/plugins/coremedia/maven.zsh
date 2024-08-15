f4efd81c108a169a4c4e21a6a9774ca629d054de
Marc Rohlfs <marc.rohlfs@coremedia.com>
2024-08-15 11:22:35 +0200

    Configure dev environment for Maven and Java

--- oh-my-zsh-custom/plugins/coremedia/maven.zsh
+++ oh-my-zsh-custom/plugins/coremedia/maven.zsh
@@ -2,3 +2,7 @@
 
 # Provide alias to execute Maven without verifying SSL certificates for artifact downloads
 alias mvn-insecure='mvn -Dmaven.resolver.transport=wagon -Dmaven.wagon.http.ssl.insecure=true'
+
+# Development environment for Maven and Java
+export _JAVA_OPTIONS='-Djava.net.preferIPv4Stack=true -Djava.awt.headless=true'
+export MAVEN_OPTS='-Xms256m -Xmx2048m -Dfile.encoding=UTF-8'
